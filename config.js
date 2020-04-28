f265dc12862c2d95b60e212be2c15214b14efc80
SamarthGVasist <46111574+SamarthGVasist@users.noreply.github.com>
2020-04-28 11:38:00 +0530

    Updated config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 var firebaseConfig = {
-    apiKey: "AIzaSyCbPpQHN-udgX1AnBbXRENTDVEmgT5o4D0",
-    authDomain: "test-16110.firebaseapp.com",
-    databaseURL: "https://test-16110.firebaseio.com",
-    projectId: "test-16110",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "test-16110.appspot.com",
     messagingSenderId: "1091965719681",
-    appId: "1:1091965719681:web:fd803272fdd9b0660d2980",
+    appId: "",
     measurementId: "G-6VV9HPSPM4"
   };
   
